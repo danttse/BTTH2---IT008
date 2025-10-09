c85ace0a086740a68c47de6a9ff9e37ddff3cc4e
Tran Thanh Dan <tranthanhdan.se@gmail.com>
2025-10-09 11:11:05 +0700

    update BAI05.cs

--- BAI05.cs
+++ BAI05.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,12 @@ namespace BTTH2
     {
         protected string diadiem;
         protected long giatien;
-        protected int dientich;
+        protected float dientich;
         public string getDiadiem
         { get { return diadiem; } }
         public long getGiatien
         { get { return giatien; } }
-        public long getDientich
+        public float getDientich
         { get { return dientich; } }
         public abstract void Nhap();
         public abstract void Xuat();
@@ -26,7 +26,7 @@ namespace BTTH2
             Console.Write("Gia tien: ");
             giatien = long.Parse(Console.ReadLine());
             Console.Write("Dien tich: ");
-            dientich = int.Parse(Console.ReadLine());
+            dientich = float.Parse(Console.ReadLine());
         }
         protected void Xuatthongtinchung()
         {
